9a3e815feb2e05106877e14bf34e7d390fa6d972
tansionline <tatarselcuk27@gmail.com>
2020-02-05 23:44:42 +0300

    hi

---
+++ js/main.js
@@ -0,0 +1,3 @@
+function script() {
+    document.getElementById("script").innerHTML = "Hello, internet's people. im selcuk tatar and im student. Sometimes i learn new something and i wanna share to internet's people. ";
+}
